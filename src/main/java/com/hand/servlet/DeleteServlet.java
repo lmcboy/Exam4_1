954eee4d2540082c8cae7f3c21654f523be5b374
mclin <457028799@qq.com>
2015-08-16 20:26:37 +0800

    commit

--- src/main/java/com/hand/servlet/DeleteServlet.java
+++ src/main/java/com/hand/servlet/DeleteServlet.java
@@ -3,7 +3,6 @@ package com.hand.servlet;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -38,21 +37,48 @@ public class DeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection conn = null;
 		try {
-			int id = Integer.parseInt(request.getParameter("id"));
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila","mc","123456");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila","mc","123456");
+			int id = Integer.parseInt(request.getParameter("id"));
 			conn.setAutoCommit(false);
-			String sql = "DELETE FROM film"
+			String sql1 = "DELETE FROM film"
 						+ " WHERE film_id = " + id;
+			
+			String sql2 = "DELETE FROM film_category"
+					+ " WHERE film_id = " + id;
+			
+			String sql3 = "DELETE FROM inventory"
+					+ " WHERE film_id = " + id;
+			
+			String sql4 = "DELETE FROM rental"
+					+ " WHERE inventory_id in (select inventory_id from inventory where film_id=" + id + ")" ;
+			
+//			String sql5 =  "DELETE FROM film_text"
+//					+ " WHERE film_id = " + id;
+//			
+//			String sql6 = "DELETE FROM payment"
+//					+ " WHERE rental_id = (select rental_id from rental"
+//					+ " WHERE inventory_id = (select inventory_id from inventory where film_id=" + id + " limit 1) limit 1)";
 			Statement st;
 			st = (Statement) conn.createStatement();
-			st.execute(sql);
+//			st.execute(sql6);
+//			st.execute(sql5);
+			st.execute(sql2);
+			st.execute(sql4);
+			st.execute(sql3);
+			st.execute(sql1);
 			conn.commit();
 			RequestDispatcher rd = request.getRequestDispatcher("/ShowServlet");
 			rd.forward(request, response);
 			conn.close();
 		} catch (ClassNotFoundException e1) {
+			try {
+				conn.rollback();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 			e1.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
